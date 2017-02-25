8f76c7a386e008bc74d81c97d8d31a7189d212a6
Shuo Shi <xxxrrtt@163.com>
2017-02-25 11:40:34 +0800

    Finish the article view!

--- shaozi_blog/views.py
+++ shaozi_blog/views.py
@@ -1,18 +1,11 @@
 from django.shortcuts import render
 from shaozi_blog import models
-def index(request):
-    recent_articles=models.Articles.objects.all()[0:5]
-    if recent_articles.count()!=0:
-        model_language_to_bootstrap_style(recent_articles)
-        return render(request,template_name='shaozi_blog/index.html',
-                context={'article_set':recent_articles,})
-    return render(request,template_name='shaozi_blog/index.html')
-
-
-def model_language_to_bootstrap_style(articles):
+from django.http import Http404
+def model_lebal_to_bootstrap_style(articles):
 #transmit color to bootstrap-style for template 
-#the color_to_html is not completed 
-    color_to_html={'b':'primary',}
+#this dict to bootstrap is stupied. rebuild it in the future
+    color_to_html={'b':'primary','a':'default','g':'success'
+            ,'sb':'info','y':'warining','r':'danger'}
     for an_article in articles:
         an_article.lang=[]
         if an_article.language:
@@ -22,13 +15,39 @@ def model_language_to_bootstrap_style(articles):
                 if a_label !='':
                     color_and_lang=a_label.split(':')
                     color_and_lang[0]=color_to_html[color_and_lang[0]]
-                    color_and_lang[1]=color_and_lang[1].center(6).replace(' ',
-                            '&nbsp;')
+                    color_and_lang[1]=color_and_lang[1].center(6).replace(
+                            ' ','&nbsp;')
                     an_article.lang.append(tuple(color_and_lang))
     return
 
+def index(request):
+
+    recent_articles=models.Articles.objects.all()[0:5]
+    if recent_articles.count()!=0:
+        model_lebal_to_bootstrap_style(recent_articles)
+        return render(request,template_name='shaozi_blog/index.html',
+                context={'article_set':recent_articles,})
+    return render(request,template_name='shaozi_blog/index.html')
+
+
 def category(request,cate):
-   return render(request,template_name='shaozi_blog/category.html')
+    cate_articles=models.Articles.objects.filter(category__contains=cate)[0:5]
+    model_lebal_to_bootstrap_style(cate_articles)
+    return render(request,
+        template_name='shaozi_blog/category.html',
+        context={'article_set':cate_articles,})
+
 def article(request,pk):
-   return render(request,template_name='shaozi_blog/article.html')
+#the an_article_set is stupid to fit the model_lebal_to_bootstrap_style func
+#and needed to rebuilded
+    try:
+        an_article_set=models.Articles.objects.filter(article_id=pk)
+        model_lebal_to_bootstrap_style(an_article_set)
+        content=an_article_set[0].path.readlines()
+        an_article_set[0].path.close()
+    except (IndexError,FileNotFoundError,AttributeError) :
+        raise Http404
+    return render(request,
+        template_name='shaozi_blog/article.html',
+        context={'an_article':an_article_set[0],'content':content})
 # Create your views here.
